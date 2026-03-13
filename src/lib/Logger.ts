76054d5fc44ba0bcbcd8a40a91611bd3a3249521
Leonardo Wiedemeier <leonardo.wiedemeier@gmail.com>
2026-03-13 10:02:18 +0100

    refactor(logger): to use typescript types instead of jsdoc

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -6,55 +6,30 @@ export {
 	error
 };
 
+type loglevel = 0 | 1 | 2 | 3 | 4 | 5;
+let LOG_LEVEL: loglevel = 5;
 
-/** @type {0 | 1 | 2 | 3 | 4 | 5} */
-let LOG_LEVEL = 5;
+const setLoglevel = (level: loglevel) => LOG_LEVEL = level;
 
-/** 
-	* @param {0 | 1 | 2 | 3 | 4 | 5} level
-	* @return void
-	* */
-const setLoglevel = level => LOG_LEVEL = level;
-
-/**
-	* @param { String } context
-	* @param { any } value
-	* @return void
-	* */
-const trace = (context, value) => {
+const trace = (context: String, value: any) => {
 	if (LOG_LEVEL <= 2) {
 		console.trace(context, value);
 	}
 };
 
-/**
-	* @param { String } context
-	* @param { any } value
-	* @return void
-	* */
-const debug = (context, value) => {
+const debug = (context: String, value: any): void => {
 	if (LOG_LEVEL <= 3) {
 		console.debug(context, value);
 	}
 };
 
-/**
-	* @param { String } context
-	* @param { any } value
-	* @return void
-	* */
-const warn = (context, value) => {
+const warn = (context: String, value: any): void => {
 	if (LOG_LEVEL <= 4) {
 		console.warn(context, value);
 	}
 };
 
-/**
-	* @param { String } context
-	* @param { any } value
-	* @return void
-	* */
-const error = (context, value) => {
+const error = (context: String, value: any): void => {
 	if (LOG_LEVEL <= 5) {
 		console.error(context, value);
 	}
